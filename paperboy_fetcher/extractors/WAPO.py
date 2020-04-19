517124f1e630627f5d5da902501dc290ffb9ec6a
Ben Follis <ben@follis.net>
2020-04-18 18:28:00 -0700

    Fix: NYT works, added WAPO

--- paperboy_fetcher/extractors/WAPO.py
+++ paperboy_fetcher/extractors/WAPO.py
@@ -1,12 +1,14 @@
 """
-As of Mid April 2020, the code below will extract the article text from NYT news articles.
+As of Mid April 2020, the code below will extract the article text from
+Note, WAPO is really stingy about giving us true data, so we probably
+will break soon.
 """
 from bs4 import BeautifulSoup
 
 def extract_article(html):
     soup = BeautifulSoup(html, 'html.parser')
     title = soup.title.string
-    body = soup.find(attrs={'itemprop': 'articleBody'})
+    body = soup.find(attrs={'class': 'article-body'})
     content = []
     content_nodes = body.find_all('p')
     for node in content_nodes:
