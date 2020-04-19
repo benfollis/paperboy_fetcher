5c09214bc2589edad83c656c08200d8e077a49f6
Ben Follis <ben@follis.net>
2020-04-18 17:09:32 -0700

    BBC works, adding CNN

--- paperboy_fetcher/extractors/CNN.py
+++ paperboy_fetcher/extractors/CNN.py
@@ -1,15 +1,14 @@
 """
-As of Mid April 2020, the code below will extract the article text from BBCs news articles. It will not remove the
-Word Have Your say subsection, but hopefully that won't prevent usage of the data.
+As of Mid April 2020, the code below will extract the article text from CNN news articles.
 """
 from bs4 import BeautifulSoup
 
 def extract_article(html):
     soup = BeautifulSoup(html, 'html.parser')
     title = soup.title.string
-    body = soup.find(attrs={'property': 'articleBody'})
+    body = soup.find(attrs={'itemprop': 'articleBody'})
     content = []
-    content_nodes = body.find_all('p')
+    content_nodes = body.find_all('div', attrs={'class': 'zn-body__paragraph'})
     for node in content_nodes:
         for chunk in node.strings:
             content.append(chunk)
